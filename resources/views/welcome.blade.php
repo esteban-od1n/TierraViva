ca5c9be5e0d657047bc0933de17b9a9aa0e59cbe
Laura Castillo <lcastillo13@ucol.mx>
2024-04-10 11:23:42 -0600

    imagenes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu principal</title>
+    <!--favicon-->
+    <link rel="shortcut icon" href="/resources/img/logos/favicon.ico" type="image/x-icon">
+    <title>Tierra Viva</title>
 </head>
 <body>
     <header>
