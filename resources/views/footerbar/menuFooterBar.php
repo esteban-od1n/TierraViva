45cd3822d7d2081ee7cc509da2d384ff2cb8bea5
Laura Castillo <lcastillo13@ucol.mx>
2024-04-24 21:57:35 -0600

    footer sin responsive

--- resources/views/footerbar/menuFooterBar.php
+++ resources/views/footerbar/menuFooterBar.php
@@ -1,21 +1,26 @@
 <!-- resources/views/menuFooterBar.blade.php -->
-<footer style="background-color: #f8f9fa; padding: 20px;">
-    <div style="display: flex; align-items: center; justify-content: space-between;">
-        <!-- Div del logo -->
-        <div>
-            <img src="" alt="Logo del portal" style="height: 100px;"> <!-- LOGO -->
+<footer class="footer">
+    <div class="grupo-1">
+        <div class="box">
+            <figure>
+                <img src="img/logos/Logo.png" alt="Logo del portal">
+            </figure>  
         </div>
-        
-        <!-- Div del texto "Lorem ipsum  " -->
-        <div>
-            <p style="margin-left: 20px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non explicabo facere excepturi perspiciatis soluta ab neque numquam possimus sapiente voluptates rerum, impedit ipsam unde, natus magnam laudantium labore consequuntur. Voluptates!</p> <!-- TEXTO -->
+        <div class="box">
+            <img src="img/logos/tierraviva.png" alt="nombre">
+            <p>En Tierra Viva podrás encontrar diferentes recursos y diversos apartados para poder acceder y crear tu propio huerto en casa.<br> Únete a nuestra comunidad y descubre cómo promover la agricultura urbana y comunidades sostenibles.</p>
+        </div>
+        <div class="box">
+            <h2>SIGUENOS</h2>
+            <div class="redsocial">
+                <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                <a href="#"><ion-icon name="logo-github"></ion-icon></a>
+            </div>
         </div>
     </div>
-    
-    <!-- Div de las redes sociales
-    <div style="display: flex; justify-content: center; margin-top: 20px;">
-        <a href="#"><img src="/ruta/al/facebook.png" alt="Facebook" style="width: 30px; height: 30px; margin-right: 10px;"></a>
-        <a href="#"><img src="/ruta/al/twitter.png" alt="Twitter" style="width: 30px; height: 30px; margin-right: 10px;"></a>
-        <a href="#"><img src="/ruta/al/instagram.png" alt="Instagram" style="width: 30px; height: 30px;"></a> 
-    </div>-->
+    <div class="grupo-2">
+        <small>&copy; 2024 <b> Tierra Viva </b> Todos los derechos reservados.</small>
+    </div>
 </footer>
