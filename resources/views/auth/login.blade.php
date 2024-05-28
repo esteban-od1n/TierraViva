1d6008ab7473ce3493e2decadef308652f859a42
Laura Castillo <lcastillo13@ucol.mx>
2024-05-28 10:11:10 -0600

    pagrina principal terminada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,97 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesión</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            background-color: #f2f2f2;
-        }
-
-        .container {
-            display: flex;
-            max-width: 90%;
-            width: 100%;
-            max-height: 90%;
-            height: 100%;
-            background-color: #fff;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        .left-section {
-            flex: 1;
-            padding: 40px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center; /* Centrar verticalmente */
-        }
-
-        .right-section {
-            flex: 1;
-            background-image: url('/img/imagenes_extras/login.jpg');
-            background-size: cover;
-            background-position: center;
-            padding: 30px;
-        }
-
-        .login-form {
-            padding: 20px;
-        }
-
-        .login-form h2 {
-            margin-bottom: 20px;
-        }
-
-        .login-form input[type="email"],
-        .login-form input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 15px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-
-        .login-form button {
-            width: 100%;
-            padding: 10px;
-            background-color: #007bff;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        .login-form a {
-            display: block;
-            text-align: right;
-            margin-bottom: 10px;
-            text-decoration: none;
-            color: #007bff;
-        }
-    </style>
+    <title>Tierra Viva</title>
+    <link rel="stylesheet" href="login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    
 </head>
 <body>
-    <div class="container">
-        <div class="left-section">
-            <div class="login-form">
-                <h2>Bienvenido a Tierra Viva</h2>
-                <form action="procesar_login.php" method="POST">
-                    <input type="email" name="email" placeholder="Correo electrónico" required>
-                    <input type="password" name="password" placeholder="Contraseña" required>
-                    <button type="submit">Iniciar sesión</button>
-                </form>
-
+    <div class="container" id="main">
+        <div class="registro">
+            <form action="procesar_registro.php" method="POST">
+                <h1>Registro</h1>
+                <div class="social-container">
+                    <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                </div>
+                <p>o usa tu Email para registrarte</p>
+                <input type="text" name="username" placeholder="Nombre de usuario" required>
+                <input type="email" name="email" placeholder="Correo electrónico" required>
+                <input type="password" name="password" placeholder="Contraseña" required>
+                <input type="password" name="confirm_password" placeholder="Confirmar contraseña" required>
+                <button type="submit">Registrarse</button>
+                <p>¿Ya tienes una cuenta? <a href="/login">Inicia sesión</a></p>
+            </form>
+        </div>
+        <div class="inicio">
+            <form action="procesar_registro.php" method="POST">
+                <h1>Inicia Sesión</h1>
+                <div class="social-container">
+                    <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                </div>
+                <p>o usa tu cuenta</p>
+                <input type="email" name="email" placeholder="Correo electrónico" required>
+                <input type="password" name="password" placeholder="Contraseña" required>
+                <button type="submit">Registrarse</button>
                 <a href=/recuperarContraseña>¿Olvidaste tu contraseña?</a>
-                <p>¿Aún no tienes cuenta? <a href="/register">Regístrate</a></p>
+                <p>¿Aún no tienes cuenta? <a href="#">Regístrate</a></p>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-left">
+                    <h1>Bienvenido de Nuevo</h1>
+                    <button id="signIn">Inicia Sesión</button>
+                </div>
+                <div class="overlay-right">
+                    <h1>Bienvenido a Tierra Viva</h1>
+                    <button id="singUp">Registrate</button>
+                </div>
             </div>
         </div>
-        <div class="right-section"></div>
     </div>
+    
+    <!--ionicons-->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script> 
 </body>
 </html>
